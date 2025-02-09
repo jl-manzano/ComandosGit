35e7151e80099e32f5d4f083742294a50ab1b082
am-lozano <am.lozano@iesnervion.es>
2025-02-09 22:34:43 +0100

    commit desde la rama AntonioManuel

---
+++ src/paquetecomandos/ramaAntonioManuel.java
@@ -0,0 +1,13 @@
+package paquetecomandos;
+
+public class ramaAntonioManuel {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
