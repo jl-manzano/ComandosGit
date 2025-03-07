abb1137bb8b0a06a02852664a95a0cf23ff5b34a
am-lozano <am.lozano@iesnervion.es>
2025-03-07 13:57:23 +0100

    solucionar conflictos desde Eclipse

--- src/paquetecomandos/HelloWorld.java
+++ src/paquetecomandos/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Soy Jose y he realizado cambios en la rama.");
 		System.out.println("Cambio hecho para solucionar el conflicto");
+		
+		System.out.println("Soy Antonio y he realizado cambios en la rama para solucionar conflictos desde Eclipse");
 	}
 }
