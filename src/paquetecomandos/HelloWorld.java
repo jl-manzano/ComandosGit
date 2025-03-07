16c381ae2f137348395611573da07941a8fecc7f
am-lozano <am.lozano@iesnervion.es>
2025-03-07 13:44:23 +0100

    cambio realizador para solucionar conflicto

--- src/paquetecomandos/HelloWorld.java
+++ src/paquetecomandos/HelloWorld.java
@@ -3,5 +3,7 @@ package paquetecomandos;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("Cambio hecho para solucionar el conflicto");
 	}
 }
