4854a1fa2f4ff078b200151732ac2d3be8fff9b4
jl-manzano <jl.manzano@iesnervion.es>
2025-03-07 13:48:32 +0100

    Cambios

--- src/paquetecomandos/HelloWorld.java
+++ src/paquetecomandos/HelloWorld.java
@@ -3,5 +3,6 @@ package paquetecomandos;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Soy Jose y he realizado cambios en la rama.");
 	}
 }
