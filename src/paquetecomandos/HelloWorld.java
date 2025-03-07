98a2d09be5b008368cd702a7637502407009212e
jl-manzano <jl.manzano@iesnervion.es>
2025-03-07 14:04:42 +0100

    Otro cambio [Jose]

--- src/paquetecomandos/HelloWorld.java
+++ src/paquetecomandos/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Soy Jose y he realizado cambios en la rama.");
 		System.out.println("Cambio hecho para solucionar el conflicto");
+		System.out.println("Soy Jose y he realizado otro cambio");
 	}
 }
